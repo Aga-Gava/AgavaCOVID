825f64e17f23017054d004368f736454211f0eb9
europokefreak <49480073+europokefreak@users.noreply.github.com>
2021-02-14 14:21:22 +0100

    Diseño de pantalla de información terminado. Añadido fragment en blanco para el formulario de envio del diagnostico.

--- app/src/main/java/com/example/agavacovid/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/agavacovid/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,7 @@
 package com.example.agavacovid.ui.dashboard;
 
+import android.graphics.text.LineBreaker;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,17 +19,25 @@ import com.example.agavacovid.R;
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private TextView descripcion;
+    private TextView tituloPolitica;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        descripcion = (TextView) root.findViewById(R.id.textView4);
+        tituloPolitica = (TextView) root.findViewById(R.id.textView5);
 
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-
+                descripcion.getText().toString().replace('\n', System.lineSeparator().toCharArray()[0]);
+                tituloPolitica.getText().toString().replace('\n', System.lineSeparator().toCharArray()[0]);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    descripcion.setJustificationMode(LineBreaker.JUSTIFICATION_MODE_INTER_WORD);
+                }
             }
         });
         return root;
